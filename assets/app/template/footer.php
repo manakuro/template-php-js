ba2297834839270454a0d494c02c72f790042c09
manakuro <a7108100@gmail.com>
2016-07-27 00:34:33 -0700

    First Version - Add php files - Add javascript files - Add css files - Add fonts files - Add images files

---
+++ assets/app/template/footer.php
@@ -0,0 +1,21 @@
+<?php 
+    use WWM\Utility;
+?>
+<footer class="page-footer">
+    
+    <div class="row">
+    </div>
+
+</footer>
+
+        <!-- Vendors -->    
+        <!-- build:js assets/dist/js/vendor.min.js -->
+        <script src="../js/libs/modernizr-2.8.3.min.js"></script>
+        <script src="../js/libs/jquery.magnific-popup.min.js"></script>
+        <!-- endbuild -->
+
+        <!-- JavaScript -->
+        <script src="assets/dist/js/bundle.js"></script>
+
+    </body>
+</html>
---
+++ assets/gulpfile.js
@@ -0,0 +1,188 @@
+var $ = require('gulp-load-plugins')(),
+    watchify = require('watchify'),
+    browserify = require('browserify'),
+    gulp = require('gulp'),
+    source = require('vinyl-source-stream'),
+    buffer = require('vinyl-buffer'),
+    assign = require('lodash.assign'),
+    glob = require('glob'),
+    browser = require("browser-sync").create(),
+    babelify = require('babelify');
+
+// Config
+var config = require('./gulpfile.config')();
+
+/**
+ *   Server Task
+ * 
+ */
+ gulp.task('server', function(){
+    browser.init({
+        proxy: config.document_root // Document root
+    });
+ });
+
+/**
+ *   JS Task
+ * 
+ */
+gulp.task('watchify', function(){
+    jsComplie(true);
+});
+
+gulp.task('browserify', function(){
+    jsComplie(false);
+});
+
+// var srcFile = glob.sync(config.js.app),
+var srcFile = glob.sync(config.js.app, { ignore : config.js.ignore }),
+    browserifyOp = {
+        cache: {},
+        packageCache: {},
+        entries: srcFile,
+        debug: true
+    },
+    options = assign({}, watchify.args, browserifyOp);
+    console.log(srcFile);
+function jsComplie(isWatch) {
+
+    var b, rebundle;
+
+    if (isWatch) {
+
+        b = watchify(browserify(options).transform(babelify));
+
+        rebundle = function () {
+            return b.bundle()
+                    .on('error', function(err){
+                        console.log($.util.colors.red(err.message));
+                        this.emit('end');
+                     })
+                    .pipe(source(config.js.bundled))
+                    .pipe(gulp.dest(config.js.dist));
+        };
+
+        b.on('update', rebundle);
+        b.on('log', $.util.log);
+
+    } else {
+
+        b = browserify(options).transform(babelify);
+
+        rebundle = function () {
+            return b.bundle()
+                    .on('error', function(err){
+                        console.log($.util.colors.red(err.message));
+                        this.emit('end');
+                     })
+                    .pipe(source(config.js.bundled))
+                    .pipe(buffer())
+                    .pipe($.sourcemaps.init({ loadMaps: true }))
+                    .pipe($.uglify())
+                    .pipe($.sourcemaps.write('./'))
+                    .pipe(gulp.dest(config.js.dist));
+        };
+
+    }
+    return rebundle();
+
+}
+
+/**
+ *   CSS Task
+ * 
+ */
+gulp.task('css', cssBundle);
+function cssBundle() {
+    return gulp.src(config.css.app)
+               // .pipe($.sass().on('error', sass.logError))
+               .pipe($.plumber({
+                    errorHander: function(err){
+                        console.log(err.messageFormatted);
+                        this.emit('end');
+                    }
+                }))
+               .pipe($.sass({outputStyle: 'compressed'}))
+               .pipe($.autoprefixer())
+               .pipe(gulp.dest(config.css.dist));
+}
+
+/**
+ *   Use Ref Task
+ *   - Optimize CSS and JavaScript files
+ * 
+ */
+
+gulp.task('useref', function(){
+    return gulp.src(config.html.app)
+               .pipe($.useref({
+                    base: '../' // Specify the output folder relative to the 'config.html.dist'
+               }))
+               .pipe($.if('*.js', $.uglify()))
+               .pipe($.if('*.css', $.cssnano()))
+               .pipe(gulp.dest(config.html.dist));
+});
+
+/**
+ *  Image Task
+ *  - Optimize the images
+ * 
+ */
+gulp.task('images', function(){
+    return gulp.src(config.img.app)
+               .pipe($.cache($.imagemin()))
+               .pipe(gulp.dest(config.img.dist));
+});
+
+/**
+ *  Fonts Task
+ *  - Copying fonts to dist
+ * 
+ */
+gulp.task('fonts', function(){
+    return gulp.src(config.fonts.app)
+               .pipe(gulp.dest(config.fonts.dist));
+});
+
+/**
+ *  Clean up files Task
+ * 
+ */
+var del = require('del');
+gulp.task('clean:dist', function(){
+    return  del.sync('dist');
+});
+
+/**
+ *  Combining Gulp Task
+ *  - Clean up generated files
+ *  - Create JS, CSS, Image, Fonts and put them into dist folder
+ *  - Optimize JS and CSS
+ * 
+ */
+var runSequence = require('run-sequence');
+gulp.task('build', function(callback){
+    runSequence('clean:dist', ['browserify', 'css', 'images', 'fonts'], 'useref', callback);
+});
+
+gulp.task('load', function(){
+    gulp.watch('./app/*.php', ['useref']);
+    gulp.watch('./app/**', function(){
+        browser.reload();
+    });
+});
+
+/**
+ *  Watching Task
+ *  - Reload server
+ *  - Watch task with JS and CSS
+ * 
+ */
+gulp.task('watch', ['watchify'], function(){
+    gulp.watch(config.css.app, ['css']);
+});
+
+// Default 
+gulp.task('default', function(callback){
+    runSequence(['watch'], callback);
+});
